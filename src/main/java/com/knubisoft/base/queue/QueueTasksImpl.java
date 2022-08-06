f915d0a40e2b2d98e21db170c051acd3d5e80e31
Nimikin <82108826+Nimikin@users.noreply.github.com>
2022-08-06 21:52:38 +0300

    Add files via upload

--- src/main/java/com/knubisoft/base/queue/QueueTasksImpl.java
+++ src/main/java/com/knubisoft/base/queue/QueueTasksImpl.java
@@ -1,36 +1,85 @@
 package com.knubisoft.base.queue;
 
 import com.knubisoft.base.queue.car.Car;
+import com.knubisoft.base.queue.car.CarComparator;
 
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class QueueTasksImpl implements QueueTasks {
 
     @Override
     public Queue<Integer> reverseQueueUsingRecursion(Queue<Integer> queue) {
-        return null;
+        if (queue.isEmpty()) {
+            return queue;
+        }
+        int curr = queue.poll();
+        reverseQueueUsingRecursion(queue);
+        queue.add(curr);
+        return queue;
     }
 
     @Override
     public Queue<Integer> reverseFirstKElementsOfQueue(Queue<Integer> queue, int k) {
-        return null;
+        if (queue.isEmpty() || k > queue.size()) {
+            return queue;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            stack.push(queue.poll());
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+        for (int i = 0; i < queue.size() - k; i++) {
+            queue.add(queue.poll());
+        }
+        return queue;
     }
 
     @Override
     public Queue<Integer> sortQueueOfInt(Queue<Integer> queue) {
-        return null;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            temp.add(queue.poll());
+        }
+        Collections.sort(temp);
+        for (int i = 0; i < temp.size(); i++) {
+            queue.add(temp.get(i));
+        }
+        return queue;
     }
 
     @Override
     public boolean validParentheses(String parentheses) {
-        return false;
+        Deque result = new LinkedList();
+        for (int j = 0; j < parentheses.length(); j++) {
+            char ch = parentheses.charAt(j);
+            switch (ch) {
+                case '{':
+                case '[':
+                case '(':
+                    result.push(ch);
+                    break;
+                case '}':
+                case ']':
+                case ')':
+                    if (!result.isEmpty()) {
+                        char chx = result.pop().toString().charAt(0);
+                        if ((ch == '}' && chx != '{') ||
+                                (ch == ']' && chx != '[') ||
+                                (ch == ')' && chx != '('))
+                            return false;
+                    }
+            }
+        }
+        return true;
     }
 
     @Override
     public PriorityQueue<Car> implementPriorityQueueThroughComparator(List<Car> cars) {
-        return null;
+        PriorityQueue<Car> carsPriority = new PriorityQueue<>(cars.size(), new CarComparator());
+        carsPriority.addAll(cars);
+        return carsPriority;
     }
 
 }
